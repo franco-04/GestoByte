4c7c842cca09b3b11b65076337c1f42a174a2a1f
Franco-04 <davidfranco7329@gmail.com>
2025-07-21 17:47:30 -0600

    vista estudiantes

--- Frontend/src/pages/Admin/views/Estudiantes.jsx
+++ Frontend/src/pages/Admin/views/Estudiantes.jsx
@@ -37,9 +37,17 @@ export default function Estudiantes() {
     return <p>No hay estudiantes en esta carrera.</p>;
 
   return (
-    <div className="coming-soon">
-      <h1>Gestión pendiente Pene</h1>
-      <p>Esta sección estará disponible próximamente</p>
+    <div className="estudiantes-container">
+      <h1 className="estudiantes-title">Estudiantes de la carrera {carrera}</h1>
+      <ul className="estudiantes-list">
+        {estudiantes.map(e => (
+          <li key={e.id_usuario} className="estudiante-card">
+            <p className="estudiante-nombre">{e.nombre} {e.apellido}</p>
+            <p className="estudiante-id">ID: {e.id_usuario}</p>
+            <p className="estudiante-id">{e.email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
